3f53591f6235bcb09f9e61b25a627cfdfbe43977
dinocuzy <binh222120@gmail.com>
2025-10-03 16:19:37 +0700

    updated lab 2 bootstrap slot 7

--- slot7/lab2/src/interface/AppNavbar.js
+++ slot7/lab2/src/interface/AppNavbar.js
@@ -1,8 +1,8 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
 function AppNavbar() {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Container>
+        <Navbar bg="dark" variant="dark" expand="lg" >
+            <Container style={{maxWidth:"80%"}}>
                 <Navbar.Brand href="#">🍕 Pizza Shop</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
@@ -12,8 +12,8 @@ function AppNavbar() {
                         <Nav.Link href="#">Contact</Nav.Link>
                     </Nav>
                     <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-danger" type="submit">Search</button>
+                        <input className="form-control" style={{width:"300px"}} type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-danger" type="submit">🔍</button>
                     </form>
                 </Navbar.Collapse>
             </Container>
